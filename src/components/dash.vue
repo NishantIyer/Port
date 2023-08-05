79204f785d5eebce275fbcb6bc5505e57ffa7016
Nishant Iyer <88764215+NishantIyer@users.noreply.github.com>
2023-08-05 07:33:04 +0530

    Update dash.vue

--- src/components/dash.vue
+++ src/components/dash.vue
@@ -15,7 +15,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url('https://pbs.twimg.com/media/DVHhlY9XcAEIntE?format=jpg&name=4096x4096');
+  background-image: url('https://media.discordapp.net/attachments/971299427715272734/1137203762054840330/image.png?width=531&height=378');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
